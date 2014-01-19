7412ab61dd4a78426e3686dde0033df3bf98e472
Dave Winer <dave.winer@gmail.com>
2014-01-19 11:32:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-var http = require ('http');
-var counter = 0;
-var server = http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	var color = 'blue';
-	if (counter % 2 === 1) {
-		color = 'no, green';
-		}
-	response.end(color);
-	counter++;
-	});
-server.listen (1337);
+console.log ("Hi Mom!")
