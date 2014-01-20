aa06ab98bd107f6c2825058bad48ab0b8b6620fb
Dave Winer <dave.winer@gmail.com>
2014-01-19 23:33:01 -0500

    Update

--- brentserver.js
+++ brentserver.js
@@ -22,21 +22,6 @@ var isAlpha = function (ch) {
 	return (((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z')));
 	}
 
-var readHttp = function (server, path, callback) {
-	var options = {
-		host: "tmp.scripting.com",
-		path: "/testing/0012.txt"
-		};
-	http.request (options, function (response) {
-		var httpresult = "";
-		response.on ("data", function (chunk) {
-			httpresult += chunk;
-			});
-		response.on ("end", function () {
-			console.log ('readHttp: " + httpresult);
-			});
-		}).end ();
-	}
 
 var writeStaticFile = function (path, data, type, acl) {
 	var bucketname = "";
@@ -69,7 +54,6 @@ var writeStaticFile = function (path, data, type, acl) {
 		});
 	}
 
-readHttp ();
 console.log ("starting server");
 var counter = 0;
 var server = http.createServer(function(request, response) {
