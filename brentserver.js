d2680269890d12b4a6f2c18e8ca9f455b49bac6f
Dave Winer <dave.winer@gmail.com>
2014-01-19 22:27:45 -0500

    Update

--- brentserver.js
+++ brentserver.js
@@ -1,34 +1,28 @@
+var AWS = require ("aws-sdk");
+var s3 = new AWS.S3 ();
+var http = require ('http');
 
-var writeStaticFile = function (path, filetext, type) {
+var writeStaticFile = function (path, data, type, acl) {
 	var bucketname = "tmp.scripting.com";
 	if (type == undefined) {
 		type = "text/plain";
 		}
+	if (acl == undefined) {
+		acl = "public-read";
+		}
 	var params = {
-		ACL: "public-read",
+		ACL: acl,
 		ContentType: type,
-		Body: filetext,
+		Body: data,
 		Bucket: bucketname,
 		Key: path
 		};
 	s3.putObject (params, function (err, data) { 
-		
 		console.log ("Wrote S3 file: http://" + bucketname + "/" + path);
-		
 		});
 	}
 
-
-console.log ("Brent's server app");
-var AWS = require ("aws-sdk");
-console.log ("AWS initialized");
-var s3 = new AWS.S3 ();
-console.log ("S3 initialized");
-
-writeStaticFile ("testing/helloJitsu.txt", "hello world");
-
-console.log ("Entering loop.");
-var http = require ('http');
+console.log ("starting server");
 var counter = 0;
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
