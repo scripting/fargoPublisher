425718e1ce9a7c90705cac5908ff770f8d53e3e3
Dave Winer <dave.winer@gmail.com>
2014-01-19 19:15:29 -0500

    Update

--- brentserver.js
+++ brentserver.js
@@ -1,3 +1,22 @@
+console.log ("Brent's server app");
+var AWS = require ("aws-sdk");
+console.log ("AWS initialized");
+var s3 = new AWS.S3 ();
+console.log ("S3 initialized");
+//create a new object at http://tmp.scripting.com/testing/helloJitsu.txt containing "hello world"
+	var params = {
+		ACL: "public-read",
+		ContentType: "text/plain",
+		Body: "hello world",
+		Bucket: "tmp.scripting.com",
+		Key: "testing/helloJitsu.txt"
+		};
+	s3.putObject (params, function (err, data) { 
+		console.log ("S3 object put");
+		console.log (err);
+		console.log (data);
+		});
+console.log ("Entering loop");
 var http = require ('http');
 var counter = 0;
 var server = http.createServer(function(request, response) {
