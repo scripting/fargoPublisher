95b5caeadde14ef2841043d588498fc80a26ca06
Dave Winer <dave.winer@gmail.com>
2014-01-23 16:29:16 -0500

    Update

--- publisher.js
+++ publisher.js
@@ -1,7 +1,7 @@
 //Copyright 2014, Small Picture, Inc.
-	//Last update: 1/23/2014; 4:20:15 PM Eastern.
+	//Last update: 1/23/2014; 4:28:45 PM Eastern.
 
-var myVersion = "0.54";
+var myVersion = "0.54"; 
 
 var s3HostingPath = process.env.fpHostingPath; //where we store all the users' HTML and XML files
 var s3defaultType = "text/plain";
